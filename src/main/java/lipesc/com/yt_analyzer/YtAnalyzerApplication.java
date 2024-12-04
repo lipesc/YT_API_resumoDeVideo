7093031cb99015b6dadedbdad779b13c1c51631c
lipesc <felipedrhouse@hotmail.com>
2024-12-04 19:34:16 -0300

    inicio projeto

---
+++ src/main/java/lipesc/com/yt_analyzer/YtAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package lipesc.com.yt_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lipesc/com/yt_analyzer/YtAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package lipesc.com.yt_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
